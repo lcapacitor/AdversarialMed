d14dff19aabe879bed529dc3188e7d24c0be4c39
lcapacitor <leolee63@icloud.com>
2019-04-13 21:10:13 -0400

    add vae_conv and training script

---
+++ Attack_scripts/VAE/train.py
@@ -0,0 +1,227 @@
+import os
+import cv2
+import copy
+import math
+import time
+import torch
+import joblib
+import argparse
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from tqdm import tqdm
+from vae_conv import VAE
+from torchvision import transforms
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class ImageDataset(Dataset):
+	'''
+	clean_dir:
+		(str) path of the root folder of all clean images
+	adv_dir:
+		(str) path to the root folder of all attacked images
+	attack_type:
+		(str) type of attack name
+	transform:
+		(transforms) transforms for data
+	is_train:
+		(bool) if the data for training or test
+	'''
+	def __init__(self, clean_dir, adv_dir, attack_type, transform, setName):
+		self.ori_fileList = []
+		self.adv_fileList = []
+		ori_folder = os.path.join(clean_dir, setName)
+		adv_folder = os.path.join(adv_dir, attack_type, setName)
+		for path, subdirs, files in os.walk(ori_folder):
+			for f in files:
+				self.ori_fileList.append(os.path.join(path, f))
+				adv_fname = 'adv_' + f
+				self.adv_fileList.append(os.path.join(adv_folder, adv_fname))
+		assert len(self.ori_fileList)==len(self.adv_fileList)
+		self.transform = transform
+
+	def __len__(self):
+		return len(self.ori_fileList)
+
+	def __getitem__(self, index):
+		ori_img_np = cv2.imread(self.ori_fileList[index])
+		adv_img_np = cv2.imread(self.adv_fileList[index])
+		x = self.transform(adv_img_np)
+		y = self.transform(ori_img_np)
+		assert x.shape == y.shape
+		return x, y
+
+
+def train(clean_dir, adv_dir, attack_type):
+	'''
+	clean_dir:
+		(str) path of the root folder of all clean images
+	adv_dir:
+		(str) path to the root folder of all attacked images
+	attack_type:
+		(str) type of attack name
+	'''
+	# Ignore all warnings
+	import warnings
+	warnings.filterwarnings("ignore")
+
+	# Setup Model hyer-param
+	z_size = 1024
+	hidden_dim = 128
+	drop_p = 0.5
+	image_size = 224
+	channel_num = 3
+	is_res = True
+
+	# Set up training hyer-params
+	lr = 1e-3
+	weight_decay = 1e-5
+	batch_size = 100
+	num_epochs = 50
+	best_loss = math.inf
+	loss_record = {'train': {'total_loss': [], 'rec_loss':[], 'kl_loss':[]},
+ 				   'val':   {'total_loss': [], 'rec_loss':[], 'kl_loss':[]}}
+
+ 	# Set up dataset and loader
+	data_transforms = {
+ 		'train': transforms.Compose([
+            transforms.ToPILImage(), # convert the loaed image from numpay array to PIL Image that is required by transforms.Resize
+            transforms.Resize((image_size, image_size)),
+            transforms.RandomRotation(degrees=10),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor(),
+            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]),
+        'val': transforms.Compose([
+            transforms.ToPILImage(), # convert the loaed image from numpay array to PIL Image that is required by transforms.Resize
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]),
+        }
+
+	dataset = {x: ImageDataset(clean_dir, adv_dir, attack_type, data_transforms[x], x) for x in ['train', 'val']}
+	dataset_sizes = {x: len(dataset[x]) for x in ['train', 'val']}
+	print('Dataset size: train {}, val {}'.format(dataset_sizes['train'], dataset_sizes['val']))
+
+	dataloaders = {'train': DataLoader(dataset['train'], batch_size=batch_size, shuffle=True,  num_workers=0),
+                   'val'  : DataLoader(dataset['val'],   batch_size=batch_size, shuffle=False, num_workers=0)}
+
+    # Initialize VAE model, optimizer and scheduler
+	model = VAE(image_size, channel_num, hidden_dim, z_size, is_res, drop_p).to(device)
+	optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=weight_decay)
+	scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=5, threshold=1e-7)
+
+    # Training
+	print ('Start training on {}...'.format(device))
+	since = time.time()
+
+	for epoch in range(num_epochs):
+		print('\nEpoch {}/{}, lr: {}, wd: {}'.format(epoch + 1, num_epochs,
+			  optimizer.param_groups[0]['lr'], weight_decay))
+		print('-' * 30)
+
+		for phase in ['train', 'val']:
+			if phase == 'train':
+				model.train()
+			else:
+				model.eval()   # Set model to evaluate mode
+
+            # Initial running loss
+			running_total_loss = 0.0
+			running_rec_loss = 0.0
+			running_kl_loss = 0.0
+
+			for inputs, targets in tqdm(dataloaders[phase], desc='{} iterations'.format(phase), leave=False):
+				inputs  = inputs.to(device)
+				targets = targets.to(device)
+
+            	# forward-prop
+				with torch.set_grad_enabled(phase == 'train'):
+					(mean, logvar), reconstructed = model(inputs)
+					rec_loss = model.reconstruction_loss(reconstructed, inputs)
+					kl_loss = model.kl_divergence_loss(mean, logvar)
+					total_loss = rec_loss + kl_loss
+
+                    # backward + optimize only if in training phase
+					if phase == 'train':
+						# zero the parameter gradients
+						optimizer.zero_grad()
+						# backward-prop
+						total_loss.backward()
+						optimizer.step()
+
+				# compute loss for running loss
+				running_kl_loss += kl_loss.item() * inputs.size(0)
+				running_rec_loss += rec_loss.item() * inputs.size(0)
+				running_total_loss += total_loss.item() * inputs.size(0)
+
+			# Compute epoch loss
+			epoch_kl_loss = running_kl_loss / dataset_sizes[phase]
+			epoch_rec_loss = running_rec_loss / dataset_sizes[phase]
+			epoch_total_loss = running_total_loss / dataset_sizes[phase]
+
+			# Update loss records
+			loss_record[phase]['total_loss'].append(epoch_total_loss)
+			loss_record[phase]['rec_loss'].append(epoch_rec_loss)
+			loss_record[phase]['kl_loss'].append(epoch_kl_loss)
+
+			# Output training/val results
+			print('{} Loss: total: {:.4f}, rec_loss: {:.4f}, kl_loss: {:.4f}'
+				.format(phase, epoch_total_loss, epoch_rec_loss, epoch_kl_loss))
+
+			# Step optimizer scheduler
+			if phase == 'val':
+				scheduler.step(epoch_total_loss)
+
+			# Copy best model
+			if phase == 'val' and epoch_total_loss < best_loss:
+				best_loss = epoch_total_loss
+				best_model_wts = copy.deepcopy(model.state_dict())
+
+	# End of training, return the best model
+	time_elapsed = time.time() - since
+	print('\nTraining complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+	print('Best val loss: {}'.format(best_loss))
+
+	# Save the best weights and loss_records
+	save_path = './trained_weights/'
+	if not os.path.isdir(save_path):
+		os.mkdir(save_path)
+	weight_fname = 'vae_{}_zdim{}_hdim{}_e{}_lr{}.torch'.format(attack_type, z_size, hidden_dim, num_epochs, str(lr).split('.')[-1])
+	s_path = os.path.join(save_path, weight_fname)
+	torch.save(best_model_wts, s_path)
+	print ('Best weight save to:', s_path)
+
+	save_path = './trained_records/'
+	if not os.path.isdir(save_path):
+		os.mkdir(save_path)
+	weight_fname = 'vae_{}_zdim{}_hdim{}_e{}_lr{}.pkl'.format(attack_type, z_size, hidden_dim, num_epochs, str(lr).split('.')[-1])
+	s_path = os.path.join(save_path, weight_fname)
+	torch.save(best_model_wts, s_path)
+	print ('Training records save to:', s_path)
+
+
+def main(args):
+	clean_dir = args.clean_dir
+	adv_dir = args.adv_dir
+	attack_type = args.attack_type
+	train(clean_dir, adv_dir, attack_type)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--clean_dir', type=str, required=True, help='root folder of the clean images')
+	parser.add_argument('--adv_dir', type=str, required=True, help='root folder of the adversarial images')
+	parser.add_argument('--attack_type', type=str, required=True, help='type of attacks, e.g. fgsm, b_iter')
+	args = parser.parse_args()
+	main(args)
